ab6f0790281148f27d0ffaf28602789d8f50d0b0
Vinicius Pacheco <vfpweb@gmail.com>
2019-07-16 01:07:45 -0300

    end chapter 11

--- forca.c
+++ forca.c
@@ -1,43 +1,74 @@
 #include <stdio.h>
 #include <string.h>
 
+char palavrasecreta[20];
+char chutes[26];
+int tentativas = 0;
+
+
+void abertura()
+{
+    printf("/*****************/\n");
+    printf("/* Jogo de Forca */\n");
+    printf("/*****************/\n\n");
+}
+
+void escolhepalavra()
+{
+	sprintf(palavrasecreta , "MELANCIA");
+}
+
+void chuta()
+{
+	char chute;
+	printf("Qual letra? ");
+	scanf(" %c", &chute);
+
+	chutes[tentativas] = chute;
+}
+
+int jachutou(char letra)
+{
+	int achou = 0;
+
+	for(int j = 0; j < tentativas; j++) {
+		if (chutes[j] == letra) {
+           achou = 1;
+           break;
+        }
+	}
+
+	return achou;
+}
+
+void desenhaforca()
+{
+	printf("Você já de %d chutes\n", tentativas);
+
+	for (int i = 0; i < strlen(palavrasecreta); i++) {
+        if (jachutou(palavrasecreta[i])) {
+            printf("%c ", palavrasecreta[i]);
+        } else {
+            printf("_ ");
+        }
+    }
+    printf("\n");
+}
+
 int main()
 {
-    char palavrasecreta[20];
-    sprintf(palavrasecreta, "MELANCIA");
-
     int acertou = 0;
     int enforcou = 0;
 
-    char chutes[26];
-    int tentativas = 0;
-
+    abertura();
+	escolhepalavra();
 
     do {
-        for (int i = 0; i < strlen(palavrasecreta); i++) {
 
-            int achou = 0;
-            for (int j = 0; j < tentativas; j++ ) {
-                if (chutes[j] == palavrasecreta[i]) {
-                    achou = 1;
-                    break;
-                }
-            }
-            if (achou) {
-                printf("%c ", palavrasecreta[i]);
-            } else {
-                printf("_ ");
-            }
-        }
-        printf("\n");
+		desenhaforca();
+        chuta();
 
-        char chute;
-        printf("Qual letra? ");
-        scanf(" %c", &chute);
-
-        chutes[tentativas] = chute;
-        tentativas++;
-
+		tentativas++;
     } while (!acertou && !enforcou);
     return 0;
 }
